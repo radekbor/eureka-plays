8eda54e74cfc2a926cce3d813ab51d3e7e7b1384
radekbor <radoslaw.borowiecki@gmail.com>
2018-10-14 21:04:56 +0200

    Replace restTemplate by feignClient

--- atm-machine/src/main/java/org/radekbor/atmmachine/AtmResource.java
+++ atm-machine/src/main/java/org/radekbor/atmmachine/AtmResource.java
@@ -1,12 +1,9 @@
 package org.radekbor.atmmachine;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 
@@ -14,10 +11,10 @@ import java.math.BigDecimal;
 public class AtmResource {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private BalanceClient balanceClient;
 
     @GetMapping("/{id}")
     public BigDecimal getBalance(@PathVariable("id") String id) {
-        return restTemplate.getForObject("http://Accounts/balance/" + id, BigDecimal.class);
+        return balanceClient.getBalance(id);
     }
 }
---
+++ atm-machine/src/main/java/org/radekbor/atmmachine/BalanceClient.java
@@ -0,0 +1,15 @@
+package org.radekbor.atmmachine;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(value = "accounts")
+public interface BalanceClient {
+
+    @GetMapping("/balance/{id}")
+    BigDecimal getBalance(@PathVariable("id") String id);
+
+}
